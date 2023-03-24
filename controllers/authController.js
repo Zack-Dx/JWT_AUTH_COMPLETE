1b6dde3d9725082d13259e17b0e0e2a92e3cf827
THEALAN <105835098+Zack-Dx@users.noreply.github.com>
2023-03-24 14:24:45 +0530

    User Registration Controller ready,

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,50 @@
-// Model Import
-import UserModel from "../models/User.js";
+import bcrypt from "bcryptjs";
+import UserModel from "../models/User.js"; // Model Import
 
-// Controller
 class AuthController {
   static async userSignup(req, res) {
     try {
-      console.log("hit");
-    } catch (error) {}
+      const { username, email, password } = req.body;
+
+      // Validation
+      if (!username || !email || !password) {
+        return res
+          .status(400) // Bad Request
+          .send({ success: false, message: "All fields are required." });
+      }
+
+      // Existing User Check
+      const existingUser = await UserModel.findOne({ email });
+      if (existingUser) {
+        return res
+          .status(409) // Conflict
+          .send({ success: false, message: "User is already registered." });
+      }
+
+      // Hashing User Password
+      const hashPass = await bcrypt.hash(password, 10);
+
+      // New User Registration
+      const user = await new UserModel({
+        username,
+        email,
+        password: hashPass,
+      });
+
+      await user.save();
+      return res
+        .status(201) // Created
+        .send({
+          success: true,
+          message: "User registered successfully.",
+          user,
+        });
+    } catch (error) {
+      console.log(error);
+      return res
+        .status(500) // Internal Server Error
+        .send({ success: false, message: "Something went wrong", error });
+    }
   }
   static async userLogin(req, res) {
     try {
--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const userSchema = new mongoose.Schema(
   {
-    name: {
+    username: {
       type: String,
       required: true,
     },
